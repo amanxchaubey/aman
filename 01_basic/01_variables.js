6546c85d5695e8a5b5afd66f2c3d27b2eff9604b
amanxchaubey <131151223+amanxchaubey@users.noreply.github.com>
2024-09-12 07:29:25 +0000

    container config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12233
+let accountEmail = "aman@gmail.com"
+var accountPassword = "12345"
+accountCity = "delhi"
+let accountState; 
+
+//accountId = 2 not allowed
+
+accountEmail = "aman@google.com"
+accountPassword = " aman2004"
+accountCity = "mumbai"
+
+  
+console.table([accountId,accountEmail,accountPassword, accountCity]) 
