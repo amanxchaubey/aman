488245ae54e59c902be95d2558c9dc1389f8e8f6
amanxchaubey <131151223+amanxchaubey@users.noreply.github.com>
2024-09-12 09:26:01 +0000

    comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<10);
+// console.log(2==1);
+// console.log(2 != 1); 
+
+console.log("2" > 1); //true
+console.log("02" > 1); //true
+
+console.log(null > 0); //false
+console.log(null == 0); // false
+console.log(null >= 0); //true
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+
+
+
