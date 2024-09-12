8db22650d0d479cac051743548ca6c4273d0aade
amanxchaubey <131151223+amanxchaubey@users.noreply.github.com>
2024-09-12 08:39:07 +0000

    operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,24 +1,60 @@
 let score = "33abc"
 
-console.log(typeof score)
+// console.log(typeof score)
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 //"33" -> 33
 // "33abc" -> NaN
 //true ->1; false->0
 
 let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn); 
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); 
 
 // 1->true; 0->false
 //""->false
 //"aman"->true
 
 let someNumber = 23
-let stringNumber = string(someNumber)
-console.log(stringNumber);
+// let stringNumber = string(someNumber)
+// console.log(stringNumber);
+
+// **************** Operations ***************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue) 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " aman"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); //32
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
