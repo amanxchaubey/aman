2d431f57a5a74011cf1720c4672fab1fb94bbfd7
amanxchaubey <131151223+amanxchaubey@users.noreply.github.com>
2024-09-12 07:56:03 +0000

    container config done

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"
+
+console.log(3+3)
+
+console.log("aman")
+
+let name = "aman"
+let age = 20
+let isLoggedIn = false
+
+//number -> range-> 2^53 
+console.log(typeof "aman")
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object;
+
