4d4efb34a1db17635cef2c8bea565c5c9d588097
amanxchaubey <131151223+amanxchaubey@users.noreply.github.com>
2024-09-14 20:33:48 +0000

    math and num

---
+++ 01_basic/06_nums_and_maths.js
@@ -0,0 +1,41 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+ 
+// console.log(balance.toString());
+
+// console.log(balance.toFixed(2));
+ 
+const otherNumber = 23.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//*************MATHS*********************/
+
+// console.log(Maths);
+// console.log(Maths.abs(-4));
+// console.log(Maths.round(4.6));
+// console.log(Maths.ceil(4.2));
+// console.log(Maths.floor(4.9));
+// console.log(Maths.min(2,3,4,5));
+// console.log(Maths.max(4,2,5,6));
+
+console.log(Maths.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max - min + 1))+ min);
+
+
+
+
+
+
+
+
