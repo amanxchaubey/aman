780680f16ab11cc162815ec24bbc3b85aae1cf7f
amanxchaubey <131151223+amanxchaubey@users.noreply.github.com>
2024-09-15 15:48:59 +0000

    array2

---
+++ 02_basic/02_array.js
@@ -0,0 +1,27 @@
+const m_heros = ["thor","ironman", "loki"]
+const dc_heros = ["batman", "falsh", "superman"]
+
+// m_heros.push(dc_heros)
+
+// console.log(m_heros);
+
+// const allHeros = m_heros.concat(dc_heros)
+// console.log(allHeros);
+
+// const all_new_heros = [...m_heros,...dc_heros]
+// console.log(all_new_heros);
+
+// const another_array = [1,2,3,[4,5,6],7, [6,7,[4,5]]]
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+
+console.log(Array.isArray("aman"));
+console.log(Array.from("aman"));
+
+let score1 = 12
+let score2 = 15
+let score3 = 17
+let score4  = 28
+console.log(Array.of(score1,score2,score3,score4));
+
